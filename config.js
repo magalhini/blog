8cbac2478fbbfc45c88ffaf49e6fb4c24252b538
Ricardo Magalhães <magalhini@gmail.com>
2019-06-16 22:35:54 -0400

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Ricardo Magalhães',
   githubUrl: 'https://github.com/magalhini/blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'magalhini',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'magalhini',

274eeca248bc32c216c0661e9f520d6af2bc07ab
Ricardo Magalhães <magalhini@gmail.com>
2020-04-12 15:21:37 -0400

    Update styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,58 @@
-import React from 'react';
-import { graphql, Link } from 'gatsby';
+import React from 'react'
+import { graphql, Link } from 'gatsby'
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import Section from '../components/section';
-import Pills from '../components/pills';
-import MainBio from '../components/main-bio';
-import { formatPostDate, formatReadingTime } from '../utils/dates';
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Section from '../components/section'
+import Pills from '../components/pills'
+import MainBio from '../components/main-bio'
+import { formatPostDate, formatReadingTime } from '../utils/dates'
 
-import './blog-listing.css';
+import './blog-listing.css'
 
 const BlogIndexPage = ({ data: { allMdx } }) => (
-	<Layout>
-		<SEO />
-		<Section centered>
-			<MainBio />
-		</Section>
+  <Layout>
+    <SEO />
+    <Section centered>
+      <MainBio />
+    </Section>
 
-		{allMdx.nodes.map((post) => (
-			<Section key={post.fields.slug} name={post.fields.slug} centered>
-				<Link to={post.fields.slug} className="blog-listing">
-					<h2 className="blog-listing__title">{post.frontmatter.title}</h2>
-					<p className="blog-listing__date text-sc">
-						{formatPostDate(post.frontmatter.date)}
-						{` • ${formatReadingTime(post.timeToRead)}`}
-					</p>
-					<Pills items={post.frontmatter.categories} />
-					<p>{post.frontmatter.description}</p>
-				</Link>
-			</Section>
-		))}
-	</Layout>
-);
+    {allMdx.nodes.map(post => (
+      <Section key={post.fields.slug} name={post.fields.slug} centered>
+        <Link to={post.fields.slug} className="blog-listing">
+          <h2 className="blog-listing__title">{post.frontmatter.title}</h2>
+          <p className="blog-listing__date text-sc">
+            {formatPostDate(post.frontmatter.date)}
+            {` • ${formatReadingTime(post.timeToRead)}`}
+          </p>
+          <Pills items={post.frontmatter.categories} />
+          <p>{post.frontmatter.description}</p>
+        </Link>
+      </Section>
+    ))}
+  </Layout>
+)
 
-export default BlogIndexPage;
+export default BlogIndexPage
 
 export const query = graphql`
-	query BlogIndex {
-		allMdx(filter: { fields: { published: { eq: true } } }, sort: { fields: [frontmatter___date], order: DESC }) {
-			nodes {
-				fields {
-					slug
-				}
-				timeToRead
-				frontmatter {
-					title
-					description
-					categories
-					date(formatString: "MMMM DD, YYYY")
-				}
-			}
-		}
-	}
-`;
+  query BlogIndex {
+    allMdx(
+      filter: { fields: { published: { eq: true } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      nodes {
+        fields {
+          slug
+        }
+        timeToRead
+        frontmatter {
+          title
+          description
+          categories
+          date(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+  }
+`

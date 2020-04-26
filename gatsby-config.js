f8af48ec946984b42c1d3b7f3e13c009429c777d
Ricardo Magalhães <magalhini@gmail.com>
2020-04-26 14:01:29 -0400

    Analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: 'UA-41481871-4',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
@@ -23,11 +23,11 @@ module.exports = {
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
-        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
         // Enables Google Optimize Experiment ID
-        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
+        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
         // Set Variation ID. 0 for original 1,2,3....
-        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
+        // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
